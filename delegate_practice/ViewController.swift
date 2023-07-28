0db350db594e16db4dcf456c5ac40d897ef58b95
dhuiPark <hasong925@gmail.com>
2023-07-28 14:11:48 +0900

    Initial Commit

---
+++ delegate_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegate_practice
+//
+//  Created by dhui on 2023/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
